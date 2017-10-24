44683b0731b4174227a8b7a316c78e232edd789b
Thanh Phu <thanh.phu@daliworks.net>
2017-10-24 13:26:25 +0900

    Add config for containers

--- rascal-config.js
+++ rascal-config.js
@@ -2,9 +2,14 @@ module.exports = {
     vhosts: {
         '/': {
             connection: [
-                'amqp://guest:guest@localhost:5672',
-                'amqp://guest:guest@localhost:5673',
-                'amqp://guest:guest@localhost:5674'
+                // Run on real machine
+                // 'amqp://guest:guest@localhost:5672',
+                // 'amqp://guest:guest@localhost:5673',
+                // 'amqp://guest:guest@localhost:5674'
+                // Run in docker
+                'amqp://guest:guest@rabbit1:5672',
+                'amqp://guest:guest@rabbit2:5672',
+                'amqp://guest:guest@rabbit3:5672'
             ],
             exchanges: {
                 'mosca': {
